f35884f0697884ad7163a64155e632e83fdeaf51
redru <luzenobi@gmail.com>
2025-07-19 14:37:17 +0200

    Update to version 0.2.0

--- Formula/dconv.rb
+++ Formula/dconv.rb
@@ -3,8 +3,8 @@
 class Dconv < Formula
   desc "A CLI tool to convert from and to unix timestamp"
   homepage "https://github.com/redru/dconv"
-  url "https://github.com/redru/dconv/releases/download/v0.1.0/dconv-0.1.0-macos-arm64.tar.gz"
-  sha256 "e477caf052ca7a0ef8f61846b40c2ef2ec8304bca65eab145dcf089f298f5127"
+  url "https://github.com/redru/dconv/releases/download/v0.2.0/dconv-0.2.0-macos-arm64.tar.gz"
+  sha256 "2d15e768c04ecbc17fc35db05613b3e3f686b162fac8b2ad1811e674380ed782"
   license "Apache-2.0"
 
   def install
